0ba54507944bc0d513fecf33789066095ebd1b8d
shubhashish812 <ssing446@asu.edu>
2023-11-11 10:12:47 -0700

    Added database over sqlitestudio and created model for QA

--- djangoProj/polls/models.py
+++ djangoProj/polls/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    q = models.CharField(max_length=200)
+    dt = models.DateTimeField("date published")
+
+
+class Choice(models.Model):
+    q = models.ForeignKey(Question, on_delete=models.CASCADE)
+    ans = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+
