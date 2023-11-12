019b3a2d7c591119ef0498da69f2d2ffc4316788
shubhashish812 <ssing446@asu.edu>
2023-11-11 17:48:32 -0700

    Added model responses and admin superuser control

--- djangoProj/polls/models.py
+++ djangoProj/polls/models.py
@@ -1,9 +1,18 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
-    q = models.CharField(max_length=200)
+    ques = models.CharField(max_length=200)
     dt = models.DateTimeField("date published")
+
+    def __str__(self):
+        return self.ques
+
+    def is_recent(self):
+        return self.dt >= timezone.now()-datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
@@ -11,4 +20,5 @@ class Choice(models.Model):
     ans = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
-
+    def __str__(self):
+        return self.ans
