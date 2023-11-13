3ae0d8d97c09922b6bd292620514616d9b770243
shubhashish812 <ssing446@asu.edu>
2023-11-13 13:07:56 -0700

    Added templates and more views

--- djangoProj/polls/urls.py
+++ djangoProj/polls/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.index, name="index"), ]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("<int:q_id>/", views.detail, name="detail"),
+    path("<int:q_id>/results/", views.results, name="results"),
+    path("<int:q_id>/vote/", views.vote, name="vote"),
+]
--- djangoProj/polls/views.py
+++ djangoProj/polls/views.py
@@ -1,5 +1,24 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello")
+    questions = Question.objects.order_by("-dt")[:5]
+    print(questions)
+    context = {
+        "questions": questions,
+    }
+    return render(request, "polls/index.html", context)
+
+
+def detail(request, q_id):
+    return HttpResponse("Question: %s." % q_id)
+
+
+def results(request, q_id):
+    return HttpResponse("results on question: %s." % q_id)
+
+
+def vote(request, q_id):
+    return HttpResponse("voting on question: %s" % q_id)
