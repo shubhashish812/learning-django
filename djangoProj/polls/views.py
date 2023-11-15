4cbe09b98f93efe7f17f9127eb36faa92a58c003
shubhashish812 <ssing446@asu.edu>
2023-11-15 12:17:32 -0700

    Added results template and implemented voting on choices for questions

--- djangoProj/polls/views.py
+++ djangoProj/polls/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse, Http404
-from django.shortcuts import render
-from .models import Question
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from .models import Question, Choice
 
 
 def index(request):
@@ -22,8 +23,25 @@ def detail(request, q_id):
 
 
 def results(request, q_id):
-    return HttpResponse("results on question: %s." % q_id)
+    question = get_object_or_404(Question, pk=q_id)
+    return render(request, "polls/results.html", {"question": question})
 
 
 def vote(request, q_id):
-    return HttpResponse("voting on question: %s" % q_id)
+    question = get_object_or_404(Question, pk=q_id)
+    try:
+        sel_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+
+        return render(request, "polls/detail.html",
+                      {
+                          "question": question,
+                          "error_message": "Didnt select choice"
+                      },
+                      )
+    else:
+        sel_choice.votes += 1
+        sel_choice.save()
+        # Sending redirect to ensure data doesn't post twice if back button is pressed
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
