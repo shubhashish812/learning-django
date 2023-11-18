06c02916efb4d7b120cb2a01aaccb9213c9938b1
shubhashish812 <ssing446@asu.edu>
2023-11-17 19:47:57 -0700

    Added test case to ensure no future question

--- djangoProj/polls/tests.py
+++ djangoProj/polls/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+
+
+class QuestionModelTests(TestCase):
+    def test_is_recent_future_question(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_ques = Question(dt=time)
+        self.assertIs(future_ques.is_recent(), False)
