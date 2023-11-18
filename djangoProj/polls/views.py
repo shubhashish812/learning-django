38c67477ab2ed3963ebd688ba0e8463cfe0d6731
shubhashish812 <ssing446@asu.edu>
2023-11-18 01:28:07 -0700

    Added CSS style for Index page

--- djangoProj/polls/views.py
+++ djangoProj/polls/views.py
@@ -20,7 +20,7 @@ class DetailView(generic.DetailView):
     template_name = "polls/detail.html"
 
     def get_queryset(self):
-        return Question.objects.filter(pub_date__lte=timezone.now())
+        return Question.objects.filter(dt__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
