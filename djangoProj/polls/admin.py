b87e6c7c804f62e78d4915e832503b4c0a515f95
shubhashish812 <ssing446@asu.edu>
2023-11-18 10:07:30 -0700

    Added Choices field and list display for admin

--- djangoProj/polls/admin.py
+++ djangoProj/polls/admin.py
@@ -1,17 +1,19 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 
-from django.contrib import admin
-from .models import Question
+
+class ChoicesInLine(admin.TabularInline):
+    model = Choice
+    extra = 2
 
 
 class QuestionAdmin(admin.ModelAdmin):
+    list_display = ["ques", "dt", "is_recent"]
     fieldsets = [
         (None, {"fields": ["ques"]}),
         ("Date information", {"fields": ["dt"]}),
     ]
+    inlines = [ChoicesInLine]
 
 
 admin.site.register(Question, QuestionAdmin)
-
-
