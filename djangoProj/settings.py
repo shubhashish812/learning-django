b42c361f2daba8a9a674ef0b4b41ed73b1011aa2
shubhashish812 <ssing446@asu.edu>
2023-11-18 12:13:01 -0700

    Added DjDT toolbar over Polls App - troubleshoot

--- djangoProj/settings.py
+++ djangoProj/settings.py
@@ -45,11 +45,11 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'djangoProj.urls'
